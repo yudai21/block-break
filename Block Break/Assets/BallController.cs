3dbf26c828840ce8b0f3f8cbe19061476209dbc9
yudai <Haman.kagoshima.kubo@gmail.com>
2024-04-25 11:58:54 +0900

    途中経過

--- Block Break/Assets/BallController.cs
+++ Block Break/Assets/BallController.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10;
+    private Vector3 objNormalVector = Vector3.zero;
+    private Rigidbody rb;
+    private Vector3 afterReflectVelocity = Vector3.zero;
     void Start()
-    {
-        Debug.Log("test");
+    { 
+        rb = GetComponent<Rigidbody>();  
+        var force = (transform.forward + transform.right) * speed;
+        rb.velocity = force;
+        afterReflectVelocity = rb.velocity;    
     }
+    
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        objNormalVector = collision.contacts[0].normal;
+        Vector3 reflectVec = 
+        Vector3.Reflect(afterReflectVelocity,objNormalVector);
+        rb.velocity = reflectVec;
+
+        afterReflectVelocity = rb.velocity;
+    }
     // Update is called once per frame
     void Update()
     {
